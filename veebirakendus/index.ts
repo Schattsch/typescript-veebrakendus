a45c283f56d9c423515531715f3d2ce1eb8e82b6
Schattsch <karelsch123@gmail.com>
2023-05-30 09:50:12 +0300

    API päringud mudeliga

--- veebirakendus/index.ts
+++ veebirakendus/index.ts
@@ -1,5 +1,5 @@
 import express, { Express, Request, Response } from "express";
-import stringsController from "./controllers/strings";
+import productsController from "./controllers/products";
 
 const app: Express = express();
 
@@ -7,7 +7,7 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
-app.use('/', stringsController);
+app.use(productsController);
 
 app.listen(3000,() => {
     console.log(`[server]: Server is running at http://localhost:3000`);
